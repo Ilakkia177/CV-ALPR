d6ebd03a531f75e09578d16973728b3dcf4e9b7e
ILAKKIA T <ilakkiat@ILAKKIAs-Laptop.local>
2025-11-06 13:10:00 +0530

    comment added

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,5 +1,6 @@
 from car_detection import run_pipeline
 
+# give all imp paths and start the pipeline for alpr.
 if __name__ == "__main__":
     video_path = "data/cropped_10.mp4"
     templates_path = "templates/char_templates_hybrid"
